b44bcf9a8c6119d76e04d4b0289599393fec21d3
DAVID BARLOW <davidbarlow@DAVIDs-MacBook-Pro-2.local>
2019-03-08 14:30:08 -0800

    updated node server file to point to build index.html file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const publicPath=path.join(__dirname,'..','public');
+const publicPath=path.join(__dirname,'..','build');
 const port = process.env.PORT || 3001
 
 app.use(express.static(publicPath));
 
-app.get('/dashboard', (req, res) =>{
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+ });
+
+ //app.use(express.static(__dirname + '/public'));
+
+app.get('*', (req, res) =>{
     console.log(publicPath);
     res.sendFile(path.join(publicPath, 'index.html'));
 });
